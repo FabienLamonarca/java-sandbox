01e532a1c40a45dc92e3d48b3de4c65d979ab4c6
Fabien Lamonarca <fabien.lamonarca@gmail.com>
2022-02-17 16:52:52 +0100

    Init + logger

---
+++ src/main/java/com/lamonarca/Logz.java
@@ -0,0 +1,11 @@
+package com.lamonarca;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Logz {
+    public static final Logger logger = LogManager.getLogger();
+
+    private Logz() {
+    }
+}
